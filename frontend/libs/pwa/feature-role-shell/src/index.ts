acc6b7f967d548ff86def6d239c47688bc839447
8itamin <61024289+8itamin@users.noreply.github.com>
2026-09-04 21:49:15 +0700

    perf: remove app-shell logout from lazy feature barrel

--- frontend/libs/pwa/feature-role-shell/src/index.ts
+++ frontend/libs/pwa/feature-role-shell/src/index.ts
@@ -1,6 +1,5 @@
 export * from './lib/auth.component';
 export * from './lib/auth-action.component';
-export * from './lib/authorized-account-menu.component';
 export * from './lib/client-today.component';
 export * from './lib/client-workout-history.component';
 export * from './lib/client-workout-list.component';
